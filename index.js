e889446e4887abd9d615976f525eb61b3fc67caa
Burton Parker <burtonparker@gmail.com>
2022-03-01 13:05:04 -0600

    Node MongoDB Example 2 - working

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const dbname = 'nucampsite';
 
 MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => { // later versions of node driver won't require true, the true flag is suggested by the dev team.
 
-    assert.strictEqual(err, null); // the first argument here is the actual value that we're checking, the second argument is the expected value that we're checking against to see if the first argument strictly equals the second. if the actual AND expected values match, then we'll continue on. however, if they don't match, (if err is not strictly equal to null) then this assert will fail. when an assert fails, it will throw an error and terminate the ENTIRE application and console logs the error that occured. if the assert DOES NOT fail, then everything is fine and the application continues...
+    assert.strictEqual(err, null); // the first argument here is the actual value that we're checking, the second argument is the expected value that we're checking against to see if the first argument strictly equals the second. if the actual AND expected values match, then we'll continue on. however, if they don't match, (if err is not strictly equal to null) then this assert will fail. when an assert fails, it will throw an error and terminate the ENTIRE application and console logs the error that occured. if the assert DOES NOT fail, then everything is fine and the application continues....
 
     console.log('Connected correctly to server');
 
